f58d4cec824f31562c4dbbb472a0784953a49359
Josh Trommel <joshtrommel@gmail.com>
2016-04-30 17:32:29 -0700

    Improved prefixing and changed colours

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', ['sass']);
 
@@ -7,8 +8,17 @@ gulp.task('watch', function() {
 	gulp.watch('stylesheets/style.scss', ['sass'])
 })
 
+gulp.task('prefix', function() {
+	return gulp.src('stylesheets/style.css')
+		.pipe(autoprefixer({
+			browsers: ['last 12 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('stylesheets'))
+})
+
 gulp.task('sass', function() {
 	return gulp.src('stylesheets/style.scss')
-	.pipe(sass())
-	.pipe(gulp.dest('stylesheets'))
+		.pipe(sass())
+		.pipe(gulp.dest('stylesheets'))
 });
